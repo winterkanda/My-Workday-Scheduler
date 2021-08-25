404d0f51737110188e05f6ce4fed7b5d9a5cee39
Kiran Kanda <kkanda@dons.usfca.edu>
2021-08-24 20:14:47 -0700

    fixed minor mistakes on html and added in a save icon

--- script.js
+++ script.js
@@ -2,45 +2,48 @@
 $("#currentDay").text(moment().format("MMMM Do YYYY hh:mm:ss a"));
 
 // row buttons 
-$("#saveBtn").on("click", function() {
+$(".saveBtn").on("click", function() {
     var currentHour = $(this)
         .parent()
         .attr("id");
 
-    var descriptionText = $("<textarea>")
-        .attr("class", "description")
+    var descriptionText = $(this)
+        .siblings("textarea")
         .val()
-        .text()
         .trim();
 
+        console.log(descriptionText)
         localStorage.setItem(currentHour, descriptionText);
 })
 
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#hour8").children(".description").val(localStorage.getItem("hour8"));
+$("#hour9").children(".description").val(localStorage.getItem("hour9"));
+$("#hour10").children(".description").val(localStorage.getItem("hour10"));
+$("#hour11").children(".description").val(localStorage.getItem("hour11"));
+$("#hour12").children(".description").val(localStorage.getItem("hour12"));
+$("#hour13").children(".description").val(localStorage.getItem("hour13"));
+$("#hour14").children(".description").val(localStorage.getItem("hour14"));
+$("#hour15").children(".description").val(localStorage.getItem("hour15"));
+$("#hour16").children(".description").val(localStorage.getItem("hour16"));
+$("#hour17").children(".description").val(localStorage.getItem("hour17"));
 
 //
 function hours() {
-    var currentTime = time().hour();
+    var currentTime = parseInt(moment().format("hh"))
         $(".hour-block").each(function () {
 
         var blockTime = parseInt($(this)
             .attr("id").split("hour")[1]);
+
+            console.log(currentTime)
+            console.log(blockTime)
         if (blockTime < currentTime) {
             $(this).addClass("past");
                 $(this).removeClass("future");
                 $(this).removeClass("present");
         } else if (blockTime === currentTime) {
-            $(this).addClass("past");
-                $(this).removeClass("present");
+            $(this).addClass("present");
+                $(this).removeClass("past");
                 $(this).removeClass("future");
         } else {
             $(this).removeClass("present");
